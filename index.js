8119ca2285381e8d3a0feb5074ccc10b6beafd65
matheusaleso <oliveirasalesmatheus@gmail.com>
2021-06-04 22:31:37 -0300

    ads

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 
-app.listen(3000);
+//app.listen(3000);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+const INVALIDATE = "invalidate";
+function notify(){
+    io.sockets.emit(INVALIDATE, 1);
+}
+server.listen(3000);
 app.get('/', function(req, res){res.send('Hello World')});
 
 /* 
@@ -16,19 +23,19 @@ Servidor
 
 const usuarios = [
     {id: 0, name: "Matheus Sales Oliveira", email: "m203577@dac.unicamp.br", 
-    favoriteSerie: "The Boys", password: "abcldldl", GenreAction: 1,
-    GenreAdventure: 0, GenreComedy: 0, GenreDrama: 1, GenreFantasy: 1,
-    GenreHorror: 0, GenreMusical: 0 },
+    favoriteSerie: "The Boys", password: "abcldldl", action: 1,
+    adventure: 0, comedy: 0, drama: 1, fantasy: 1,
+    horror: 0, musical: 0 },
 
     {id: 1, name: "Gabriella Carlos", email: "g245677@dac.unicamp.br", 
-    favoriteSerie: "Game of Thrones", password: "ssssss", GenreAction: 0,
-    GenreAdventure: 1, GenreComedy: 1, GenreDrama: 0, GenreFantasy: 1,
-    GenreHorror: 0, GenreMusical: 0 },
+    favoriteSerie: "Game of Thrones", password: "ssssss", action: 0,
+    adventure: 1, comedy: 1, drama: 0, fantasy: 1,
+    horror: 0, musical: 0 },
 ]
 const endpoint = "/usuarios";
 
 app.get(endpoint, function(req,res){
-    res.send(usuarios)
+    res.send(usuarios.filter(Boolean));
 });
 
 app.get(`${endpoint}/:id`, function(req,res){
@@ -43,22 +50,52 @@ app.get(`${endpoint}/:id`, function(req,res){
 
 })
 
-app.post(endpoint, (req,res) =>{
+app.post(endpoint, (req, res) => {
     const usuario = {
-        id : usuario.lenght,
+        id : usuarios.length,
         name : req.body["name"],
         email: req.body["email"],
         favoriteSerie: req.body["favoriteSerie"],
         password: req.body["password"],
-        GenreAction: req.body["GenreAction"],
-        GenreAdventure: req.body["GenreAdventure"],
-        GenreComedy: req.body["GenreComedy"],
-        GenreDrama: req.body["GenreDrama"],
-        GenreFantasy: req.body["GenreFantasy"],
-        GenreHorror: req.body["GenreHorror"],
-        GenreMusical: req.body["GenreMusical"],
+        action: req.body["action"],
+        adventure: req.body["adventure"],
+        comedy: req.body["comedy"],
+        drama: req.body["drama"],
+        fantasy: req.body["fantasy"],
+        horror: req.body["horror"],
+        musical: req.body["musical"],
     };
     usuarios.push(usuario);
     res.send("1");
-})
+    notify();
+});
 
+app.put(`${endpoint}/:id`, (req, res) => {
+   const id = req.params.id;
+   const usuario = {
+       id: id,
+       name : req.body["name"],
+       email: req.body["email"],
+       favoriteSerie: req.body["favoriteSerie"],
+       password: req.body["password"],
+       GenreAction: req.body["GenreAction"],
+       GenreAdventure: req.body["GenreAdventure"],
+       GenreComedy: req.body["GenreComedy"],
+       GenreDrama: req.body["GenreDrama"],
+       GenreFantasy: req.body["GenreFantasy"],
+       GenreHorror: req.body["GenreHorror"],
+       GenreMusical: req.body["GenreMusical"]
+   };
+
+   usuarios[id] = usuario;
+   res.send("1");
+   notify();
+});
+
+app.delete(`${endpoint}/:id`, (req, res) => {
+    const id = req.params.id;
+    delete usuarios[id];
+ 
+    res.send("1");
+    notify();
+ });
